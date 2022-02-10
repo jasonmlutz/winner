4a97bd7655187d13dff773952bdba9789ed7f654
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 10:28:21 -0800

    Move css content to assets

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,8 +4,6 @@ import { useRoutes } from "react-router-dom";
 import Home from "./Home";
 import NewSurveyForm from "./Surveys/NewSurveyForm";
 
-import "./App.css";
-
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
