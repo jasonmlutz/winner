7e930426444db6bdf6b94d8a6ffdd67a0ddd22b6
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 10:56:08 -0800

    Build SurveysContainer

--- app/javascript/components/Surveys/SurveysContainer.jsx
+++ app/javascript/components/Surveys/SurveysContainer.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const SurveysContainer = () => {
-  return <div>SurveysContainer</div>;
+  const [surveys, setSurveys] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`/api/v1/surveys/`);
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const data = await response.json();
+      if (!data) {
+        window.alert(`No surveys found!`);
+        return;
+      }
+
+      setSurveys(data);
+    }
+
+    fetchData();
+    return;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [navigate]);
+
+  const renderSurveys = () => {
+    if (surveys.length) {
+      return (
+        <>
+          <div className="heading">Existing surveys</div>
+          <ul>
+            {surveys.map((survey) => (
+              <li
+                key={survey.id}
+                className="text__icon"
+                onClick={() => {
+                  navigate(`/surveys/${survey.id.toString()}`);
+                }}
+              >
+                {survey.title}
+              </li>
+            ))}
+          </ul>
+        </>
+      );
+    } else {
+      return <div className="heading">No surveys!</div>;
+    }
+  };
+
+  return <div className="SurveysContainer">{renderSurveys()}</div>;
 };
 
 export default SurveysContainer;
