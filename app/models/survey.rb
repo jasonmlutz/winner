df2bbacb067fcb99bc9b0580ad1d4931fc0496ee
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 11:55:33 -0800

    Annotate Survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: surveys
+#
+#  id         :uuid             not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Survey < ApplicationRecord
 end
