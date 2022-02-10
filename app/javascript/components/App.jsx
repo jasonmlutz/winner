93bbf0abf2ffe2a5877a3434fc8bd0c136071585
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 10:53:06 -0800

    Import and route SurveyDisplay

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,11 +3,13 @@ import { useRoutes } from "react-router-dom";
 
 import Home from "./Home";
 import NewSurveyForm from "./Surveys/NewSurveyForm";
+import SurveyDisplay from "./Surveys/SurveyDisplay";
 
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/surveys/new", element: <NewSurveyForm /> },
+    { path: "/surveys/:id", element: <SurveyDisplay /> },
   ]);
 
   return <div className="App">{routes}</div>;
