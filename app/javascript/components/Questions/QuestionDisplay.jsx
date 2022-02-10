6f64998cb8fa7aa3dd05208426bf7256c40f08d0
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 14:38:06 -0800

    Build QuestionDisplay
    Note that there are no icons at this time; neither
    IconInterface nor ObjectActions have been created.
    
    Nor is there ResponseOptionDisplay.

--- app/javascript/components/Questions/QuestionDisplay.jsx
+++ app/javascript/components/Questions/QuestionDisplay.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router";
 
-const QuestionDisplay = () => {
-  return <div>QuestionDisplay</div>;
+// import ResponseOptionsContainer from "../ResponseOptions/ResponseOptionsContainer";
+// import IconInterface from "../IconInterface";
+
+// import {
+//   handleEditSubmit,
+//   handleMove,
+//   handleDelete,
+// } from "../../resources/ObjectActions";
+
+const QuestionDisplay = ({ question, questions }) => {
+  const [editActive, setEditActive] = useState(false);
+  const [title, setTitle] = useState(question.title);
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
+
+  const path = `/surveys/${question.parentId}/${Date.now()}`;
+
+  useEffect(() => {
+    if (editActive) {
+      inputRef.current.focus();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editActive]);
+
+  const renderQuestion = () => {
+    if (editActive) {
+      return (
+        <form className="input">
+          <input
+            type="text"
+            className="input__box input__box--medium"
+            ref={inputRef}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button
+            className="input__submit input__submit--right-anchor input__submit--medium"
+            type="submit"
+            onClick={(e) =>
+              handleEditSubmit(
+                e,
+                question._id,
+                "questions",
+                title,
+                navigate,
+                path,
+                setEditActive
+              )
+            }
+          >
+            GO
+          </button>
+        </form>
+      );
+    } else {
+      return (
+        <div className="text__title text__title--medium">
+          <div className="text__title--icons-right">{question.title}</div>
+          {/* <IconInterface
+            position={question.position}
+            siblingCount={questions.length}
+            setEditActive={setEditActive}
+            handleMove={handleMove.bind(
+              null,
+              question,
+              questions,
+              "questions",
+              navigate,
+              path
+            )}
+            handleDelete={handleDelete.bind(
+              null,
+              question._id,
+              question,
+              questions,
+              "questions",
+              navigate,
+              path
+            )}
+          /> */}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="QuestionDisplay">
+      {renderQuestion()}
+      {/* <ResponseOptionsContainer
+        parentId={question._id}
+        grandparentId={question.parentId}
+      /> */}
+    </div>
+  );
 };
 
 export default QuestionDisplay;
