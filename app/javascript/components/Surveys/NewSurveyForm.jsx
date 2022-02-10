e1a42af38db66bcfa3ca2c4d91ce4636ce58fd4d
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 10:35:48 -0800

    Add x-csrf-token header to NewSurveyForm

--- app/javascript/components/Surveys/NewSurveyForm.jsx
+++ app/javascript/components/Surveys/NewSurveyForm.jsx
@@ -9,11 +9,14 @@ const NewSurveyForm = () => {
   async function handleSubmit(e) {
     e.preventDefault();
     if (title.length) {
+      // get the csrf token
+      const token = document.querySelector("[name=csrf-token]").content;
       // send the post request
       await fetch("/api/v1/surveys", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "X-CSRF-TOKEN": token,
         },
         body: JSON.stringify({
           title: title,
