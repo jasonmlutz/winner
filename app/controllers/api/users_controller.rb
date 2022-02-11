723a2b75f8ce530ea0a0eae45ccad0cb2d25349d
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-11 13:53:16 -0800

    Build users controller actions
    index, show, create, destroy
    
    index and show make use of User#protect
    (see 021b6e611ccedcf9a5890cfe7fe3bc3ab3ee1f35).
    
    post requires password and password confirmation for creation.
    
    delete makes use of authenticate_user private method
    
    All four controller actions were manually tested using Postman,
    which required diabling csrf protection; this dev change was
    reverted.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,51 @@
+class Api::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :authenticate_user, only: [:destroy]
+  wrap_parameters false
+
+  # GET /api/users
+  def index
+    @users = User.all.map { |user| user.protect }
+    render json: @users
+  end
+
+  # GET /api/users/:id(.:format)
+  def show
+    render json: @user.protect
+  end
+
+  # POST /api/users(.:format)
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user.protect
+   else
+     render json: {message: "error: user not created"}
+   end
+  end
+
+  # DELETE /api/users/:id(.:format)
+  def destroy
+    if @user && @user.destroy
+      render json: {message: "user deleted"}
+    else
+      render json: {message: "unable to delete user"}
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:name, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authenticate_user
+    @user = User.find(params[:id])&.authenticate(params[:password])
+    puts @user
+  end
+
+end
