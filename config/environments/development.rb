88ddb18089a4e2a991843d69056998d3596e5cc1
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-10 19:27:28 -0800

    Add disable-forgery-protection option
    Commented out in config/environments/development.rb,
    includes why it would be uncommented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Uncomment to disable forgery-protection in development
+  # For example, to manually test controller actions with Postman
+  # config.action_controller.default_protect_from_forgery = false
 end
