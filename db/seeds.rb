11f1cdf832d104ab3dccc36ba5cc10646f809ec0
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-11 13:29:57 -0800

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+  {name: "jason", password: "password"},
+  {name: "jarmo", password: "password"},
+  {name: "jasonmlutz", password: "password"},
+  {name: "jmlutz", password: "password"},
+  {name: "jlutz", password: "password"}
+])
