200aa3ab1fc53de1904ebe2b588f04ed3337231c
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-11 16:09:47 -0800

    Add faker gem and implement in seeds
    The seed data creates:
    - 5 users with random first names and password="password"
    - 5 polls with randomly selected author and Big Lebowski
    quotes as titles
    - between 3 and 5 questions for each survey, with Harry Potter
    quotes for questions
    - between 3 and 5 response options for each questions, with
    Star Wars quotes as titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,69 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-users = User.create([
-  {name: "jason", password: "password"},
-  {name: "jarmo", password: "password"},
-  {name: "jasonmlutz", password: "password"},
-  {name: "jmlutz", password: "password"},
-  {name: "jlutz", password: "password"}
-])
+# clear the unique value generator
+
+puts "building seed data"
+puts ""
+
+Faker::Name.unique.clear
+Faker::Movies::Lebowski.unique
+User.destroy_all
+Survey.destroy_all
+puts "reset and clear completed"
+
+# generate 5 users with random names, and default passwords
+(1..5).each do
+  User.create(
+    name:Faker::Name.unique.first_name,
+    password: "password",
+    password_confirmation: "password",
+  )
+end
+puts "#{User.count} users created"
+
+# create 5 surveys, with a randomly selected author
+(1..5).each do
+  survey = Survey.create(
+    title: Faker::Movies::Lebowski.unique.quote,
+    author_id: User.all.sample.id,
+  )
+end
+
+# loop over all the surveys
+Survey.all.each do |survey|
+  # this survey has between 3 and 5 questions
+  (1..rand(3..5)).each do |i|
+    question = Question.create(
+      title: Faker::Movies::HarryPotter.unique.quote,
+      position: i,
+      parent_id: survey.id
+    )
+  end
+  # clear the record of unique values; 
+  # different surveys may have overlap in questions
+  Faker::Movies::HarryPotter.unique.clear
+end
+
+# loop over all the questions
+Question.all.each do |question|
+  # this question has between 3 and 5 response options
+  (1..rand(3..5)).each do |j|
+    ResponseOption.create(
+      title: Faker::Movies::StarWars.unique.quote,
+      position: j,
+      parent_id: question.id,
+      grandparent_id: question.parent_id
+    )
+  end
+  # clear the record of unique values; 
+  # different questions may have overlap in response options
+  Faker::Movies::StarWars.unique.clear
+end
+
+puts "#{Survey.count} surveys created"
+puts "#{Question.count} questions created"
+puts "#{ResponseOption.count} response options created"
+puts ""
+puts "complete!"
+
