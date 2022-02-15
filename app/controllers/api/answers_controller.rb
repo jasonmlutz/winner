7252dabfd5e1af77be803b4b2e3dc56e6357620d
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-15 12:44:03 -0800

    Build Responses & Answers controller actions

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,4 +1,27 @@
 class Api::AnswersController < ApplicationController
+  before_action :set_response, only: [:index]
+  wrap_parameters false
   # POST /api/responses/:response_id/answers
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      render json: @answer
+    else
+      render json: {message: "unable to create answer"}
+    end
+
   # GET /api/responses/:response_id/answers
+  def index
+    @answers = @response.answers
+    render json: @answers
+  end
+
+  private
+    def answer_params
+      params.permit(:response_id, :response_option_id)
+    end
+
+    def set_response
+      @response = Response.find(params[:response_id])
 end
--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -1,5 +1,39 @@
 class Api::ResponsesController < ApplicationController
+  before_action :set_response, only: [:show, :destroy]
+  wrap_parameters false
   # POST /api/responses
+  def create
+    @response = Response.new(response_params)
+    if @response.save
+      render json: @response
+    else
+      render json: {message: "error: response not created"}
+    end
+  end
+
   # GET /api/responses/:id
+  def show
+    if @response
+      render json: @response
+    else
+      render json: {message: "error: no response found"}
+    end
+
   # DELETE /api/responses/:id
+  def destroy
+    if @response && @response.destroy
+      render json: {message: "response deleted"}
+    else
+      render json: {message: "unable to delete response"}
+    end
+  end
+
+  private
+    def response_params
+      params.permit(:survey_id, :respondent_id)
+    end
+
+    def set_response
+      @response = Response.find(params[:id])
+    end
 end
