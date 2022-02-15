8d2a0409f7bae1698c76da8d03b009f23d7d6bf5
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-15 15:37:23 -0800

    Build NewResponse
    It displays Questions and ResponseOptions using styling
    that matches SurveyDisplay and its children.

--- app/javascript/components/Responses/NewResponse.jsx
+++ app/javascript/components/Responses/NewResponse.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const NewResponse = () => {
   const params = useParams();
   const survey_id = params.survey_id.toString();
+
+  const navigate = useNavigate();
 
   const [survey, setSurvey] = useState({});
   const [questions, setQuestions] = useState({});
@@ -35,14 +37,78 @@ const NewResponse = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <div>
-      <div>Live Survey Display</div>
-      <div>title: {survey.title}</div>
-      <div>number of questions: {questions.length}</div>
-      <div>number of response options: {responseOptions.length}</div>
-    </div>
-  );
+  const renderQuestions = () => {
+    const sortedQuestions = questions.sort((a, b) => a.position - b.position);
+    return (
+      <ul>
+        {sortedQuestions.map((question) => (
+          <li key={question.id}>
+            <div className="QuestionDisplay">
+              <div className="text__title text__title--small">
+                {question.title}
+              </div>
+              <div className="ResponseOptionsContainer">
+                {renderResponseOptions(question)}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderResponseOptions = (question) => {
+    const filteredSortedResponseOptions = responseOptions
+      .filter((element) => element.parent_id === question.id)
+      .sort((a, b) => a.position - b.position);
+    return (
+      <ul>
+        {filteredSortedResponseOptions.map((responseOption) => (
+          <li key={responseOption.id}>
+            <div className="ResponseOptionDisplay text__title text__title--small">
+              {responseOption.title}
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  if (
+    Object.keys(survey).length &&
+    Object.keys(questions).length &&
+    Object.keys(responseOptions).length
+  ) {
+    return (
+      <div className="SurveyDisplay">
+        <div className="text__title text__title--large">{survey.title}</div>
+        <div className="text__title--small">
+          Author:{" "}
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate(`/users/${survey.author_id}`);
+            }}
+          >
+            {survey.author_name}
+          </a>
+          <div className="QuestionsContainer">{renderQuestions()}</div>
+        </div>
+        <button className="input__submit input__submit--large input__submit--wide">
+          SUBMIT
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="SurveyDisplay">
+        <div className="text__title text__title--small">
+          Data not fully loaded for survey
+        </div>
+      </div>
+    );
+  }
 };
 
 export default NewResponse;
