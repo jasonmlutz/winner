ac81cf8cddd8dcddb1837e00f64a31bb048e735a
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-17 14:56:44 -0800

    Add titles w/ Helmet
    Login, Register, UserDisplay

--- app/javascript/components/Sessions/NewSessionForm.jsx
+++ app/javascript/components/Sessions/NewSessionForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router";
+import { Helmet } from "react-helmet";
 
 const NewSessionForm = () => {
   const inputRef = useRef();
@@ -58,72 +59,77 @@ const NewSessionForm = () => {
   }
 
   return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      <img
-        src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-        className="absolute h-full w-full object-cover"
-      />
-      <div className="inset-0 bg-black opacity-50 absolute"></div>
-      <div className="container mx-auto relative z-10 mt-20 w-4/5 md:w-3/5 lg:w-2/5 xl:w-2/5">
-        <div className="flex flex-col content-center relative z-10">
-          <div className="self-center mb-2 text-xl font-light text-gray-800 dark:text-white">
-            Welcome back!
-          </div>
-          <span className="self-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-            Need to crreate an account?{" "}
-            <a
-              href=""
-              className="text-sm text-blue-500 underline hover:text-blue-700"
-              onClick={(e) => {
-                e.preventDefault();
-                navigate("/register");
-              }}
-            >
-              Register
-            </a>
-          </span>
-          <div className="p-6 mt-8">
-            <form action="#">
-              <div className="flex flex-col mb-2">
-                <div className="relative ">
-                  <input
-                    type="text"
-                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-                    placeholder="Name"
-                    value={name}
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                  />
+    <>
+      <Helmet>
+        <title>Login - Winner</title>
+      </Helmet>{" "}
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+          className="absolute h-full w-full object-cover"
+        />
+        <div className="inset-0 bg-black opacity-50 absolute"></div>
+        <div className="container mx-auto relative z-10 mt-20 w-4/5 md:w-3/5 lg:w-2/5 xl:w-2/5">
+          <div className="flex flex-col content-center relative z-10">
+            <div className="self-center mb-2 text-xl font-light text-gray-800 dark:text-white">
+              Welcome back!
+            </div>
+            <span className="self-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
+              Need to crreate an account?{" "}
+              <a
+                href=""
+                className="text-sm text-blue-500 underline hover:text-blue-700"
+                onClick={(e) => {
+                  e.preventDefault();
+                  navigate("/register");
+                }}
+              >
+                Register
+              </a>
+            </span>
+            <div className="p-6 mt-8">
+              <form action="#">
+                <div className="flex flex-col mb-2">
+                  <div className="relative ">
+                    <input
+                      type="text"
+                      className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                      placeholder="Name"
+                      value={name}
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col mb-2">
-                <div className="relative ">
-                  <input
-                    type="password"
-                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                  />
+                <div className="flex flex-col mb-2">
+                  <div className="relative ">
+                    <input
+                      type="password"
+                      className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-full my-4">
-                <button
-                  type="submit"
-                  className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                  onClick={handleSubmit}
-                >
-                  Login
-                </button>
-              </div>
-            </form>
+                <div className="flex w-full my-4">
+                  <button
+                    type="submit"
+                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    onClick={handleSubmit}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default NewSessionForm;
--- app/javascript/components/Users/UserDisplay.jsx
+++ app/javascript/components/Users/UserDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { FiPlusCircle } from "react-icons/fi";
+import { Helmet } from "react-helmet";
 
 import Header from "../Header";
 
@@ -39,164 +40,169 @@ const UserDisplay = () => {
   }, [params.user_id, navigate]);
 
   return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      <img
-        src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-        className="absolute h-full w-full object-cover"
-      />
-      <Header />
-      <div className="flex items-start justify-between relative py-[74px]">
-        <div className="flex flex-col w-full md:space-y-4">
-          <div className="overflow-auto h-screen pb-24 px-4 md:px-6">
-            <h1 className="text-4xl font-semibold text-gray-800 dark:text-white">
-              Welcome back,{" "}
-              {user.name.charAt(0).toUpperCase() + user.name.slice(1)}!
-            </h1>
-            <h2 className="text-md text-gray-400">
-              Here's a summary of your activity.
-            </h2>
-            <div className="flex my-6 items-center w-full space-y-4 md:space-x-4 md:space-y-0 flex-col md:flex-row">
-              <div className="flex items-center w-full md:w-2/3 space-x-4">
-                <div className="w-1/2">
-                  <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
-                    <p className="text-2xl text-black dark:text-white font-bold">
-                      {authoredSurveys.length || "0"}
-                    </p>
-                    <p className="text-gray-400 text-sm">
-                      Authored survey{authoredSurveys.length === 1 ? "" : "s"}
-                    </p>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault();
-                        navigate("#");
-                      }}
-                      className="display lg:hidden rounded-full absolute p-0 bg-indigo-500 top-4 right-4 text-white text-3xl hover:bg-indigo-700"
-                    >
-                      <FiPlusCircle />
-                    </a>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault();
-                        navigate("#");
-                      }}
-                      className="hidden lg:block px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 absolute top-4 right-4 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                    >
-                      {authoredSurveys.length
-                        ? "Create another survey!"
-                        : "Create your first survey!"}
-                    </a>
-                  </div>
-                </div>
-                <div className="w-1/2">
-                  <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
-                    <p className="text-2xl text-black dark:text-white font-bold">
-                      {responses.length || "0"}
-                    </p>
-                    <p className="text-gray-400 text-sm">
-                      Response{responses.length === 1 ? "" : "s"} submitted
-                    </p>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault();
-                        navigate("/surveys");
-                      }}
-                      className="block lg:hidden rounded-full absolute p-0 bg-indigo-500 top-4 right-4 text-white text-3xl hover:bg-indigo-700"
-                    >
-                      <FiPlusCircle />
-                    </a>
-                    <a
-                      href=""
-                      onClick={(e) => {
-                        e.preventDefault();
-                        navigate("/surveys");
-                      }}
-                      className="hidden lg:block px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 absolute top-4 right-4 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                    >
-                      {responses.length
-                        ? "Find more live surveys!"
-                        : "Respond to your first survey!"}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center space-x-4"></div>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
-              <div className="w-full">
-                <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
-                  <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
-                    Your Live Surveys
-                  </p>
-                  <div className="flex items-end space-x-2 my-6">
-                    <p className="text-5xl text-black dark:text-white font-bold">
-                      {authoredSurveys.filter((elem) => elem.publish).length}
-                    </p>
-                  </div>
-                  <div className="dark:text-white">
-                    {authoredSurveys
-                      .filter((elem) => elem.publish)
-                      .map((survey) => (
-                        <div
-                          key={survey.id}
-                          className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
-                        >
-                          <p>{survey.title}</p>
-                          <div className="flex items-end text-xs">
-                            responses
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
-                  <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
-                    Your Unpublished Surveys
-                  </p>
-                  <div className="flex items-end space-x-2 my-6">
-                    <p className="text-5xl text-black dark:text-white font-bold">
-                      {authoredSurveys.filter((elem) => !elem.publish).length}
-                    </p>
-                  </div>
-                  <div className="dark:text-white">
-                    {authoredSurveys
-                      .filter((elem) => !elem.publish)
-                      .map((survey) => (
-                        <div
-                          key={survey.id}
-                          className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
-                        >
-                          <p>{survey.title}</p>
-                          <div className="flex items-end text-xs">edit</div>
-                        </div>
-                      ))}
-                  </div>
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
-                  <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
-                    Your Responses
-                  </p>
-                  <div className="flex items-end space-x-2 my-6">
-                    <p className="text-5xl text-black dark:text-white font-bold">
-                      12
-                    </p>
-                  </div>
-                  <div className="dark:text-white">
-                    {responses.map((response) => (
-                      <div
-                        key={response.id}
-                        className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
+    <>
+      <Helmet>
+        <title>{user.name} - Winner</title>
+      </Helmet>
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+          className="absolute h-full w-full object-cover"
+        />
+        <Header />
+        <div className="flex items-start justify-between relative py-[74px]">
+          <div className="flex flex-col w-full md:space-y-4">
+            <div className="overflow-auto h-screen pb-24 px-4 md:px-6">
+              <h1 className="text-4xl font-semibold text-gray-800 dark:text-white">
+                Welcome back,{" "}
+                {user.name.charAt(0).toUpperCase() + user.name.slice(1)}!
+              </h1>
+              <h2 className="text-md text-gray-400">
+                Here's a summary of your activity.
+              </h2>
+              <div className="flex my-6 items-center w-full space-y-4 md:space-x-4 md:space-y-0 flex-col md:flex-row">
+                <div className="flex items-center w-full md:w-2/3 space-x-4">
+                  <div className="w-1/2">
+                    <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                      <p className="text-2xl text-black dark:text-white font-bold">
+                        {authoredSurveys.length || "0"}
+                      </p>
+                      <p className="text-gray-400 text-sm">
+                        Authored survey{authoredSurveys.length === 1 ? "" : "s"}
+                      </p>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate("#");
+                        }}
+                        className="display lg:hidden rounded-full absolute p-0 bg-indigo-500 top-4 right-4 text-white text-3xl hover:bg-indigo-700"
                       >
-                        <p>{response.survey_title}</p>
-                        <div className="flex items-end text-xs">view</div>
-                      </div>
-                    ))}
+                        <FiPlusCircle />
+                      </a>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate("#");
+                        }}
+                        className="hidden lg:block px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 absolute top-4 right-4 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      >
+                        {authoredSurveys.length
+                          ? "Create another survey!"
+                          : "Create your first survey!"}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="w-1/2">
+                    <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                      <p className="text-2xl text-black dark:text-white font-bold">
+                        {responses.length || "0"}
+                      </p>
+                      <p className="text-gray-400 text-sm">
+                        Response{responses.length === 1 ? "" : "s"} submitted
+                      </p>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate("/surveys");
+                        }}
+                        className="block lg:hidden rounded-full absolute p-0 bg-indigo-500 top-4 right-4 text-white text-3xl hover:bg-indigo-700"
+                      >
+                        <FiPlusCircle />
+                      </a>
+                      <a
+                        href=""
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigate("/surveys");
+                        }}
+                        className="hidden lg:block px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 absolute top-4 right-4 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      >
+                        {responses.length
+                          ? "Find more live surveys!"
+                          : "Respond to your first survey!"}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center space-x-4"></div>
+              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
+                <div className="w-full">
+                  <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                    <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
+                      Your Live Surveys
+                    </p>
+                    <div className="flex items-end space-x-2 my-6">
+                      <p className="text-5xl text-black dark:text-white font-bold">
+                        {authoredSurveys.filter((elem) => elem.publish).length}
+                      </p>
+                    </div>
+                    <div className="dark:text-white">
+                      {authoredSurveys
+                        .filter((elem) => elem.publish)
+                        .map((survey) => (
+                          <div
+                            key={survey.id}
+                            className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
+                          >
+                            <p>{survey.title}</p>
+                            <div className="flex items-end text-xs">
+                              responses
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                    <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
+                      Your Unpublished Surveys
+                    </p>
+                    <div className="flex items-end space-x-2 my-6">
+                      <p className="text-5xl text-black dark:text-white font-bold">
+                        {authoredSurveys.filter((elem) => !elem.publish).length}
+                      </p>
+                    </div>
+                    <div className="dark:text-white">
+                      {authoredSurveys
+                        .filter((elem) => !elem.publish)
+                        .map((survey) => (
+                          <div
+                            key={survey.id}
+                            className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
+                          >
+                            <p>{survey.title}</p>
+                            <div className="flex items-end text-xs">edit</div>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                    <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
+                      Your Responses
+                    </p>
+                    <div className="flex items-end space-x-2 my-6">
+                      <p className="text-5xl text-black dark:text-white font-bold">
+                        12
+                      </p>
+                    </div>
+                    <div className="dark:text-white">
+                      {responses.map((response) => (
+                        <div
+                          key={response.id}
+                          className="flex items-center pb-2 mb-2 text-sm sm:space-x-12  justify-between border-b border-gray-200"
+                        >
+                          <p>{response.survey_title}</p>
+                          <div className="flex items-end text-xs">view</div>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -204,7 +210,7 @@ const UserDisplay = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
