bad34fe5bd683778a08be7148002680ddc1f5d3a
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-18 13:38:18 -0800

    Add Publish button

--- app/javascript/components/Surveys/SurveyDisplay.jsx
+++ app/javascript/components/Surveys/SurveyDisplay.jsx
@@ -133,7 +133,7 @@ const SurveyDisplay = () => {
       return;
     });
 
-    navigate(`/surveys/${id}/${Date.now()}`);
+    navigate(`/surveys/${id}`);
   }
 
   useEffect(() => {
@@ -248,6 +248,15 @@ const SurveyDisplay = () => {
                     )}
                   </li>
                   <QuestionsContainer parent_id={id} />
+                  <li>
+                    <button
+                      type="submit"
+                      className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                      onClick={handlePublish}
+                    >
+                      Publish
+                    </button>
+                  </li>
                 </ul>
               </div>
             </div>
