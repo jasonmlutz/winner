6fb3ed74e9bf988ce64cb4b9541f28d95c991dbe
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-18 11:11:19 -0800

    Style basics for NewSSurveyForm

--- app/javascript/components/Surveys/NewSurveyForm.jsx
+++ app/javascript/components/Surveys/NewSurveyForm.jsx
@@ -1,6 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, createRef } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
+import { Helmet, HelmetData } from "react-helmet-async";
+
+const helmetData = new HelmetData({});
+
+import Header from "../Header";
+import ScrollToTopButton from "../resources/ScrollToTopButton";
 
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
@@ -8,6 +14,19 @@ const NewSurveyForm = () => {
   const [title, setTitle] = useState("");
   const navigate = useNavigate();
   const { currentUser } = useContext(CurrentUserContext);
+
+  const [showScrollTopButton, setShowScrollTopButton] = useState(false);
+
+  const ref = createRef();
+
+  const [hideHeader, setHideHeader] = useState(false);
+  // var scrollTop = 0;
+  function handleScroll(e) {
+    setShowScrollTopButton(e.target.scrollTop > 300);
+    setHideHeader(e.target.scrollTop > 300);
+    // setHideHeader(e.target.scrollTop > scrollTop);
+    // scrollTop = e.target.scrollTop;
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -45,41 +64,62 @@ const NewSurveyForm = () => {
     }
   }
 
-  if (currentUser) {
-    return (
-      <div className="NewSurveyForm">
-        <div className="heading">Create a survey!</div>
-        <form className="input">
-          <input
-            type="text"
-            placeholder="Enter survey title"
-            className="input__box input__box--large"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <button
-            className="input__submit input__submit--right-anchor input__submit--large"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Go
-          </button>
-        </form>
-      </div>
-    );
-  } else {
-    return (
-      <div className="NewSurveyForm">
-        <div className="heading">Create a survey!</div>
-        <div className="text__title text__title--medium">
-          You must log in to create a survey.{" "}
-          <Link to="/login?source=new-survey">LOG IN</Link>{" "}
+  return (
+    <>
+      <Helmet helmetData={helmetData}>
+        <title>Surveys - Winner</title>
+      </Helmet>
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+          className="absolute h-full w-full object-cover"
+        />
+        <Header hideHeader={hideHeader} />
+        <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+        <div
+          ref={ref}
+          className="relative py-[74px] h-screen overflow-auto"
+          onScroll={(e) => handleScroll(e)}
+        >
+          <div className="mx-auto w-full">
+            <div className="pb-24 md:pt-12 px-4 md:px-6 flex flex-col items-center">
+              <ul className="flex flex-col w-11/12 sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
+                  <h3 className="text-lg leading-6 font-medium text-white">
+                    {currentUser ? (
+                      <form className="input">
+                        <input
+                          type="text"
+                          placeholder="Enter survey title"
+                          className="input__box input__box--large"
+                          value={title}
+                          onChange={(e) => {
+                            setTitle(e.target.value);
+                          }}
+                        />
+                        <button
+                          className="input__submit input__submit--right-anchor input__submit--large"
+                          type="submit"
+                          onClick={handleSubmit}
+                        >
+                          Go
+                        </button>
+                      </form>
+                    ) : (
+                      <div>
+                        You must log in to create a survey.{" "}
+                        <Link to="/login?source=new-survey">Sign in</Link>
+                      </div>
+                    )}
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    );
-  }
+    </>
+  );
 };
 
 export default NewSurveyForm;
