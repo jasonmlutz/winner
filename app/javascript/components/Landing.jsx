97817ce2c1def347aa749a9ab01d5df99011cca6
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-19 21:29:45 -0800

    Prevent Landing, Login, Register if CurrentUser
    navigate away unless !currentUser

--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -1,41 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 // import SvgLandscape from "./resources/Landscape";
 
+import { CurrentUserContext } from "./contexts/CurrentUserContext";
+
 const Landing = () => {
   const navigate = useNavigate();
+  const { currentUser } = useContext(CurrentUserContext);
 
-  return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      <img
-        src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-        className="absolute h-full w-full object-cover"
-      />
-      {/* <SvgLandscape className="absolute" /> */}
-      <div className="inset-0 bg-black opacity-25 absolute"></div>
-      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-        <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
-          <span className="font-bold uppercase text-yellow-400">Winner!</span>
-          <h1 className="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
-            Create surveys,
-            <br />
-            pick a winner
-          </h1>
-          <a
-            href=""
-            className="block bg-white hover:bg-gray-100 py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/register");
-            }}
-          >
-            Get started
-          </a>
+  if (!currentUser) {
+    return (
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
+        <img
+          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+          className="absolute h-full w-full object-cover"
+        />
+        {/* <SvgLandscape className="absolute" /> */}
+        <div className="inset-0 bg-black opacity-25 absolute"></div>
+        <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+          <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
+            <span className="font-bold uppercase text-yellow-400">Winner!</span>
+            <h1 className="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
+              Create surveys,
+              <br />
+              pick a winner
+            </h1>
+            <a
+              href=""
+              className="block bg-white hover:bg-gray-100 py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/register");
+              }}
+            >
+              Get started
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    navigate(`/users/${currentUser.id}`);
+    return null;
+  }
 };
 
 export default Landing;
--- app/javascript/components/Users/NewUserForm.jsx
+++ app/javascript/components/Users/NewUserForm.jsx
@@ -11,7 +11,7 @@ const NewUserForm = () => {
   const [password, setPassword] = useState("");
   const [password_confirmation, setPassword_confirmation] = useState("");
 
-  const { setCurrentUser } = useContext(CurrentUserContext);
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
 
   const navigate = useNavigate();
 
@@ -96,96 +96,101 @@ const NewUserForm = () => {
     }
   }
 
-  return (
-    <>
-      <Helmet helmetData={helmetData}>
-        <title>Register - Winner</title>
-      </Helmet>
-      <div className="bg-indigo-900 relative overflow-hidden h-screen">
-        <img
-          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-          className="absolute h-full w-full object-cover"
-        />
-        <div className="inset-0 bg-black opacity-50 absolute"></div>
-        <div className="container mx-auto relative z-10 mt-20 w-4/5 md:w-3/5 lg:w-2/5 xl:w-2/5">
-          <div className="flex flex-col content-center relative z-10">
-            <div className="self-center mb-2 text-xl font-light text-gray-800 dark:text-white">
-              Create a new account
-            </div>
-            <span className="self-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-              Already have an account?{" "}
-              <a
-                href=""
-                className="text-sm text-blue-500 underline hover:text-white"
-                onClick={(e) => {
-                  e.preventDefault();
-                  navigate(
-                    "/login" +
-                      (source === "new-survey" ? "?source=new-survey" : "")
-                  );
-                }}
-              >
-                Sign in
-              </a>
-            </span>
-            <div className="p-6 mt-8">
-              <form action="#">
-                <div className="flex flex-col mb-2">
-                  <div className="relative ">
-                    <input
-                      type="text"
-                      className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-                      placeholder="Name"
-                      value={name}
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                    />
+  if (!currentUser) {
+    return (
+      <>
+        <Helmet helmetData={helmetData}>
+          <title>Register - Winner</title>
+        </Helmet>
+        <div className="bg-indigo-900 relative overflow-hidden h-screen">
+          <img
+            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+            className="absolute h-full w-full object-cover"
+          />
+          <div className="inset-0 bg-black opacity-50 absolute"></div>
+          <div className="container mx-auto relative z-10 mt-20 w-4/5 md:w-3/5 lg:w-2/5 xl:w-2/5">
+            <div className="flex flex-col content-center relative z-10">
+              <div className="self-center mb-2 text-xl font-light text-gray-800 dark:text-white">
+                Create a new account
+              </div>
+              <span className="self-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href=""
+                  className="text-sm text-blue-500 underline hover:text-white"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate(
+                      "/login" +
+                        (source === "new-survey" ? "?source=new-survey" : "")
+                    );
+                  }}
+                >
+                  Sign in
+                </a>
+              </span>
+              <div className="p-6 mt-8">
+                <form action="#">
+                  <div className="flex flex-col mb-2">
+                    <div className="relative ">
+                      <input
+                        type="text"
+                        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => {
+                          setName(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col mb-2">
-                  <div className="relative ">
-                    <input
-                      type="password"
-                      className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                      }}
-                    />
+                  <div className="flex flex-col mb-2">
+                    <div className="relative ">
+                      <input
+                        type="password"
+                        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => {
+                          setPassword(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col mb-2">
-                  <div className="relative ">
-                    <input
-                      type="password"
-                      className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
-                      placeholder="Confirm Password"
-                      value={password_confirmation}
-                      onChange={(e) => {
-                        setPassword_confirmation(e.target.value);
-                      }}
-                    />
+                  <div className="flex flex-col mb-2">
+                    <div className="relative ">
+                      <input
+                        type="password"
+                        className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                        placeholder="Confirm Password"
+                        value={password_confirmation}
+                        onChange={(e) => {
+                          setPassword_confirmation(e.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div className="flex w-full my-4">
-                  <button
-                    type="submit"
-                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                    onClick={handleSubmit}
-                  >
-                    Register
-                  </button>
-                </div>
-              </form>
+                  <div className="flex w-full my-4">
+                    <button
+                      type="submit"
+                      className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                      onClick={handleSubmit}
+                    >
+                      Register
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    navigate(`/users/${currentUser.id}`);
+    return null;
+  }
 };
 
 export default NewUserForm;
