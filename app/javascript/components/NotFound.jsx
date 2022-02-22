745fb4539f5b96423e0c125e8bccddb91ae1e8ba
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 13:15:37 -0800

    Remove navigate from NotFound
    No longer needed after swapping to Link; see a436dea3bc02d86fd3ba37f2020fbc61ae34e6c0

--- app/javascript/components/NotFound.jsx
+++ app/javascript/components/NotFound.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NotFound = ({ path = "/" }) => {
-  const navigate = useNavigate();
-
   return (
     <div className="bg-indigo-900 relative overflow-hidden h-screen">
       <img
