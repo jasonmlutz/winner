4ca5034f75decd419201958abb3e3fa6844bdd6f
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 15:26:20 -0800

    Build loading skeleton SurveyDisplay (edit)

--- app/javascript/components/Surveys/SurveyDisplay.jsx
+++ app/javascript/components/Surveys/SurveyDisplay.jsx
@@ -291,7 +291,40 @@ const SurveyDisplay = () => {
     }
   } else {
     // fetch in progress (or very bad server error), show loading skeleton
-    return <>LOADING ...</>;
+    return (
+      <>
+        <Helmet helmetData={helmetData}>
+          <title>New Survey - Winner</title>
+        </Helmet>
+        <div className="bg-indigo-900 relative overflow-hidden h-screen">
+          <img
+            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+            className="absolute h-full w-full object-cover"
+          />
+          <Header hideHeader={hideHeader} />
+          <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+          <div
+            ref={ref}
+            className="relative py-[74px] h-screen overflow-auto"
+            onScroll={(e) => handleScroll(e)}
+          >
+            <div className="mx-auto w-full">
+              <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                  <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 opacity-90 animate-pulse mb-2 rounded-md">
+                    <div className="flex flex-row justify-between">
+                      <h3 className="text-lg leading-6 font-medium text-white">
+                        Loading ...
+                      </h3>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 };
 
