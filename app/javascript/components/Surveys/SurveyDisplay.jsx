9d61896f0e91d23a8965de082b92a06056628d8a
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 13:52:43 -0800

    Improve survey not found handling
    Note this is the 'edit survey' situation. This was done like
    user not found; see b7479ad6ecc6dbf36cb9cffd01c3e81fc52cd93e

--- app/javascript/components/Surveys/SurveyDisplay.jsx
+++ app/javascript/components/Surveys/SurveyDisplay.jsx
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  useContext,
-  createRef,
-} from "react";
+import React, { useState, useRef, useEffect, createRef } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { Helmet, HelmetData } from "react-helmet-async";
@@ -16,14 +10,12 @@ import QuestionsContainer from "../Questions/QuestionsContainer";
 import Header from "../Header";
 import ScrollToTopButton from "../resources/ScrollToTopButton";
 
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import NotFound from "../NotFound";
 
 const SurveyDisplay = () => {
-  const [survey, setSurvey] = useState("");
+  const [survey, setSurvey] = useState(null);
   const [title, setTitle] = useState("");
   const [editActive, setEditActive] = useState(false);
-
-  const { currentUser } = useContext(CurrentUserContext);
 
   const navigate = useNavigate();
 
@@ -35,12 +27,9 @@ const SurveyDisplay = () => {
   const ref = createRef();
 
   const [hideHeader, setHideHeader] = useState(false);
-  // var scrollTop = 0;
   function handleScroll(e) {
     setShowScrollTopButton(e.target.scrollTop > 300);
     setHideHeader(e.target.scrollTop > 300);
-    // setHideHeader(e.target.scrollTop > scrollTop);
-    // scrollTop = e.target.scrollTop;
   }
 
   useEffect(() => {
@@ -69,7 +58,9 @@ const SurveyDisplay = () => {
   }, [params.id, navigate]);
 
   useEffect(() => {
-    setTitle(survey.title);
+    if (survey && survey.title) {
+      setTitle(survey.title);
+    }
   }, [survey]);
 
   async function handleDelete() {
@@ -142,130 +133,149 @@ const SurveyDisplay = () => {
     }
   }, [editActive]);
 
-  if (survey.publish) {
-    return (
-      <>
-        <Helmet helmetData={helmetData}>
-          <title>New Survey - Winner</title>
-        </Helmet>
-        <div className="bg-indigo-900 relative overflow-hidden h-screen">
-          <img
-            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-            className="absolute h-full w-full object-cover"
-          />
-          <Header hideHeader={hideHeader} />
-          <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
-          <div
-            ref={ref}
-            className="relative py-[74px] h-screen overflow-auto"
-            onScroll={(e) => handleScroll(e)}
-          >
-            <div className="mx-auto w-full">
-              <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-                <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                  <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
-                    <h3 className="text-lg leading-6 font-medium text-white">
-                      This survey is live and may no longer be edited.{" "}
-                      <Link
-                        to={`/surveys/${survey.id}`}
-                        className="text-sm text-blue-500 underline hover:text-blue-700"
-                      >
-                        View live survey
-                      </Link>
-                    </h3>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Helmet helmetData={helmetData}>
-          <title>New Survey - Winner</title>
-        </Helmet>
-        <div className="bg-indigo-900 relative overflow-hidden h-screen">
-          <img
-            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-            className="absolute h-full w-full object-cover"
-          />
-          <Header hideHeader={hideHeader} />
-          <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
-          <div
-            ref={ref}
-            className="relative py-[74px] h-screen overflow-auto"
-            onScroll={(e) => handleScroll(e)}
-          >
-            <div className="mx-auto w-full">
-              <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-                <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                  <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
-                    {editActive ? (
-                      <form className="flex flex-row">
-                        <input
-                          ref={inputRef}
-                          type="text"
-                          className="flex-1 text-black rounded-md px-2 mr-2"
-                          value={title}
-                          onChange={(e) => {
-                            setTitle(e.target.value);
-                          }}
-                        />
-                        <button
-                          className="hidden md:display px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                          type="submit"
-                          onClick={handleEditSubmit}
-                        >
-                          Update
-                        </button>
-                        <button
-                          onClick={handleEditSubmit}
-                          type="submit"
-                          className="display md:hidden rounded-full p-0 bg-indigo-500 text-white text-3xl hover:bg-indigo-700"
-                        >
-                          <AiOutlineCheckCircle />
-                        </button>
-                      </form>
-                    ) : (
-                      <div className="flex flex-row justify-between">
+  if (survey) {
+    // the fetch returned a (possibly empty) object
+    if (survey.id) {
+      // a valid survey was returned;
+      // render depending on survey.publish and editActive
+      if (survey.publish) {
+        return (
+          <>
+            <Helmet helmetData={helmetData}>
+              <title>New Survey - Winner</title>
+            </Helmet>
+            <div className="bg-indigo-900 relative overflow-hidden h-screen">
+              <img
+                src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+                className="absolute h-full w-full object-cover"
+              />
+              <Header hideHeader={hideHeader} />
+              <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+              <div
+                ref={ref}
+                className="relative py-[74px] h-screen overflow-auto"
+                onScroll={(e) => handleScroll(e)}
+              >
+                <div className="mx-auto w-full">
+                  <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                    <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                      <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
                         <h3 className="text-lg leading-6 font-medium text-white">
-                          {survey.title}
+                          This survey is live and may no longer be edited.{" "}
+                          <Link
+                            to={`/surveys/${survey.id}`}
+                            className="text-sm text-blue-500 underline hover:text-blue-700"
+                          >
+                            View live survey
+                          </Link>
                         </h3>
-                        <div className="flex flex-row text-white text-xl">
-                          <AiFillEdit
-                            className="mx-2 cursor-pointer hover:text-gray-200 hover:border hover:border-gray-200 hover:rounded-md"
-                            onClick={() => {
-                              setEditActive(true);
-                            }}
-                          />
-                          <AiFillDelete
-                            className="mx-2 cursor-pointer hover:text-gray-200 hover:border hover:border-gray-200 hover:rounded-md"
-                            onClick={() => handleDelete()}
-                          />
-                        </div>
-                      </div>
-                    )}
-                  </li>
-                  <QuestionsContainer parent_id={id} />
-                  <li>
-                    <button
-                      type="submit"
-                      className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                      onClick={handlePublish}
-                    >
-                      Publish
-                    </button>
-                  </li>
-                </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </>
-    );
+          </>
+        );
+      } else {
+        return (
+          <>
+            <Helmet helmetData={helmetData}>
+              <title>New Survey - Winner</title>
+            </Helmet>
+            <div className="bg-indigo-900 relative overflow-hidden h-screen">
+              <img
+                src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+                className="absolute h-full w-full object-cover"
+              />
+              <Header hideHeader={hideHeader} />
+              <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+              <div
+                ref={ref}
+                className="relative py-[74px] h-screen overflow-auto"
+                onScroll={(e) => handleScroll(e)}
+              >
+                <div className="mx-auto w-full">
+                  <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                    <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                      <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
+                        {editActive ? (
+                          <form className="flex flex-row">
+                            <input
+                              ref={inputRef}
+                              type="text"
+                              className="flex-1 text-black rounded-md px-2 mr-2"
+                              value={title}
+                              onChange={(e) => {
+                                setTitle(e.target.value);
+                              }}
+                            />
+                            <button
+                              className="hidden md:display px-4 py-2 text-xs xl:text-sm rounded-xl text-white bg-indigo-500 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                              type="submit"
+                              onClick={handleEditSubmit}
+                            >
+                              Update
+                            </button>
+                            <button
+                              onClick={handleEditSubmit}
+                              type="submit"
+                              className="display md:hidden rounded-full p-0 bg-indigo-500 text-white text-3xl hover:bg-indigo-700"
+                            >
+                              <AiOutlineCheckCircle />
+                            </button>
+                          </form>
+                        ) : (
+                          <div className="flex flex-row justify-between">
+                            <h3 className="text-lg leading-6 font-medium text-white">
+                              {survey.title}
+                            </h3>
+                            <div className="flex flex-row text-white text-xl">
+                              <AiFillEdit
+                                className="mx-2 cursor-pointer hover:text-gray-200 hover:border hover:border-gray-200 hover:rounded-md"
+                                onClick={() => {
+                                  setEditActive(true);
+                                }}
+                              />
+                              <AiFillDelete
+                                className="mx-2 cursor-pointer hover:text-gray-200 hover:border hover:border-gray-200 hover:rounded-md"
+                                onClick={() => handleDelete()}
+                              />
+                            </div>
+                          </div>
+                        )}
+                      </li>
+                      <QuestionsContainer parent_id={id} />
+                      <li>
+                        <button
+                          type="submit"
+                          className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                          onClick={handlePublish}
+                        >
+                          Publish
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      }
+    } else {
+      // no survey found; 404
+      return (
+        <NotFound
+          path="/profile"
+          notFoundMessage="Survey not found"
+          navLinkMessage="Return to profile"
+        />
+      );
+    }
+  } else {
+    // fetch in progress (or very bad server error), show loading skeleton
+    return <>LOADING ...</>;
   }
 };
 
