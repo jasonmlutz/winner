bdeeac628cf83130a7fc04e4fae86e5d2e562bc4
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 20:40:21 -0800

    Build loading conditional ResponsesContainer
    Still need to build the loading skeleton

--- app/javascript/components/Responses/ResponsesContainer.jsx
+++ app/javascript/components/Responses/ResponsesContainer.jsx
@@ -8,7 +8,7 @@ import Header from "../Header";
 import ScrollToTopButton from "../resources/ScrollToTopButton";
 
 const ResponsesContainer = () => {
-  const [responses, setResponses] = useState([{}]);
+  const [responses, setResponses] = useState(null);
   const [showScrollTopButton, setShowScrollTopButton] = useState(false);
 
   const params = useParams();
@@ -49,60 +49,73 @@ const ResponsesContainer = () => {
     return;
   }, [navigate]);
 
-  return (
-    <>
-      <Helmet helmetData={helmetData}>
-        <title>Responses - Winner</title>
-      </Helmet>
-      <div className="bg-indigo-900 relative overflow-hidden h-screen">
-        <img
-          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-          className="absolute h-full w-full object-cover"
-        />
-        <Header hideHeader={hideHeader} />
-        <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
-        <div
-          ref={ref}
-          className="relative py-[74px] h-screen overflow-auto"
-          onScroll={(e) => handleScroll(e)}
-        >
-          <div className="mx-auto w-full">
-            <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-              <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
-                  <h3 className="text-lg leading-6 font-medium text-white">
-                    Survey: {responses[0].survey_title}
-                  </h3>
-                  <div className="text-sm text-gray-100 my-2">
-                    <Link
-                      to={`/surveys/${survey_id}`}
-                      className="text-sm text-blue-500 underline hover:text-blue-700"
-                    >
-                      View live survey
-                    </Link>
-                  </div>
-                </li>
-                {responses.map((response) => (
-                  <li
-                    key={response.id}
-                    className="border-gray-100 flex flex-row mb-2"
-                  >
-                    <Link className="w-full" to={`/responses/${response.id}`}>
-                      <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 rounded-md p-4">
-                        <div className="px-1 font-medium text-white">
-                          View the response by {response.respondent_name}
-                        </div>
+  if (responses) {
+    if (responses.length) {
+      return (
+        <>
+          <Helmet helmetData={helmetData}>
+            <title>Responses - Winner</title>
+          </Helmet>
+          <div className="bg-indigo-900 relative overflow-hidden h-screen">
+            <img
+              src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+              className="absolute h-full w-full object-cover"
+            />
+            <Header hideHeader={hideHeader} />
+            <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+            <div
+              ref={ref}
+              className="relative py-[74px] h-screen overflow-auto"
+              onScroll={(e) => handleScroll(e)}
+            >
+              <div className="mx-auto w-full">
+                <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                  <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                    <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
+                      <h3 className="text-lg leading-6 font-medium text-white">
+                        Survey: {responses[0].survey_title}
+                      </h3>
+                      <div className="text-sm text-gray-100 my-2">
+                        <Link
+                          to={`/surveys/${survey_id}`}
+                          className="text-sm text-blue-500 underline hover:text-blue-700"
+                        >
+                          View live survey
+                        </Link>
                       </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+                    </li>
+                    {responses.map((response) => (
+                      <li
+                        key={response.id}
+                        className="border-gray-100 flex flex-row mb-2"
+                      >
+                        <Link
+                          className="w-full"
+                          to={`/responses/${response.id}`}
+                        >
+                          <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 rounded-md p-4">
+                            <div className="px-1 font-medium text-white">
+                              View the response by {response.respondent_name}
+                            </div>
+                          </div>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+      );
+    } else {
+      // TODO style no responses
+      return <>NO RESPONSES</>;
+    }
+  } else {
+    // TODO style loading
+    return <>LOADING ....</>;
+  }
 };
 
 export default ResponsesContainer;
