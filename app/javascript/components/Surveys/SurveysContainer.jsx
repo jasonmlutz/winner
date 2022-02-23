f99e19ea556833c6aa17c7a4c087d61cc96c91a7
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 20:28:05 -0800

    Build loading skeleton SurveysContainer

--- app/javascript/components/Surveys/SurveysContainer.jsx
+++ app/javascript/components/Surveys/SurveysContainer.jsx
@@ -100,8 +100,66 @@ const SurveysContainer = () => {
     }
   } else {
     // fetch in progress or bad server error; render loading
-    // TODO style loading
-    return <>LOADING</>;
+    return (
+      <>
+        <Helmet helmetData={helmetData}>
+          <title>loading ...</title>
+        </Helmet>
+        <div className="bg-indigo-900 relative overflow-hidden h-screen">
+          <img
+            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+            className="absolute h-full w-full object-cover"
+          />
+          <Header hideHeader={hideHeader} />
+          <div className="relative py-[74px] h-screen overflow-auto">
+            <div className="mx-auto w-full">
+              <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                  <li className="border-gray-100 flex flex-row mb-2">
+                    <div className="w-full">
+                      <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 opacity-90 rounded animate-pulse p-4 h-[100px]">
+                        <div className="px-1 flex flex-col">
+                          <div className="font-medium text-white">
+                            Loading ...
+                          </div>
+                          <div className="text-sm italic text-gray-100 my-2">
+                            {""}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="border-gray-100 flex flex-row mb-2">
+                    <div className="w-full">
+                      <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 opacity-90 rounded animate-pulse p-4 h-[100px]">
+                        <div className="px-1 flex flex-col">
+                          <div className="font-medium text-white">{""}</div>
+                          <div className="text-sm italic text-gray-100 my-2">
+                            {""}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="border-gray-100 flex flex-row mb-2">
+                    <div className="w-full">
+                      <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 opacity-90 rounded animate-pulse p-4 h-[100px]">
+                        <div className="px-1 flex flex-col">
+                          <div className="font-medium text-white">{""}</div>
+                          <div className="text-sm italic text-gray-100 my-2">
+                            {""}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 };
 
