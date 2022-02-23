3b8afa2d06aebb329f4640138eac514c116c855e
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-23 10:25:38 -0800

    Add Layout to NewResponse

--- app/javascript/components/Responses/NewResponse.jsx
+++ app/javascript/components/Responses/NewResponse.jsx
@@ -1,31 +1,22 @@
-import React, { useState, useEffect, useContext, createRef } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { Helmet, HelmetData } from "react-helmet-async";
 
-const helmetData = new HelmetData({});
-
-import Header from "../Header";
-import ScrollToTopButton from "../resources/ScrollToTopButton";
 import NotFound from "../NotFound";
+import Layout from "../Layout";
 
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 const NewResponse = () => {
+  const helmetData = new HelmetData({});
+
   const [survey, setSurvey] = useState(null);
   const [questions, setQuestions] = useState(null);
   const [responseOptions, setResponseOptions] = useState(null);
   const [answers, setAnswers] = useState({});
 
-  const [showScrollTopButton, setShowScrollTopButton] = useState(false);
-  const ref = createRef();
-
-  const [hideHeader, setHideHeader] = useState(false);
-  function handleScroll(e) {
-    setShowScrollTopButton(e.target.scrollTop > 300);
-    setHideHeader(e.target.scrollTop > 300);
-  }
-
   const { currentUser } = useContext(CurrentUserContext);
+
   const params = useParams();
   const survey_id = params.survey_id.toString();
 
@@ -169,62 +160,49 @@ const NewResponse = () => {
 
   const renderLoading = () => {
     return (
-      <>
+      <Layout>
         <Helmet helmetData={helmetData}>
           <title>loading ...</title>
         </Helmet>
-        <div className="bg-indigo-900 relative overflow-hidden h-screen">
-          <img
-            src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-            className="absolute h-full w-full object-cover"
-          />
-          <Header hideHeader={hideHeader} />
-          <div
-            ref={ref}
-            className="relative py-[74px] h-screen overflow-auto"
-            onScroll={(e) => handleScroll(e)}
-          >
-            <div className="mx-auto w-full">
-              <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-                <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                  <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 opacity-90 rounded animate-pulse shadow mb-2 rounded-md h-[100px]">
-                    <h3 className="text-lg leading-6 font-medium text-white">
-                      Loading ...
-                    </h3>
-                    <div className="mt-1 max-w-2xl text-sm text-gray-200 italic">
-                      {""}
-                      <div className="text-blue-500 underline hover:text-blue-700">
-                        {""}
-                      </div>
-                    </div>
-                    <div className="mt-1 max-w-2xl text-sm text-gray-200 italic">
-                      <div className="text-blue-500 underline hover:text-blue-700">
-                        {""}
-                      </div>
-                    </div>
-                  </li>
-                  <li className="border-gray-100 flex flex-row mb-2">
-                    <div className="w-full shadow select-none bg-gray-800 opacity-90 rounded animate-pulse rounded-md p-2 md:p-4 h-[100px]">
-                      <div className="px-1 flex flex-col">
-                        <div className="font-medium text-white">{""}</div>
-                      </div>
-                      <div className="w-full flex flex-column justify-start w-full bg-gray-500 text-gray-100 rounded-md my-4 p-2 h-[60px]"></div>
-                    </div>
-                  </li>
-                  <li className="border-gray-100 flex flex-row mb-2">
-                    <div className="w-full shadow select-none bg-gray-800 opacity-90 rounded animate-pulse rounded-md p-2 md:p-4 h-[100px]">
-                      <div className="px-1 flex flex-col">
-                        <div className="font-medium text-white">{""}</div>
-                      </div>
-                      <div className="w-full flex flex-column justify-start w-full bg-gray-500 text-gray-100 rounded-md my-4 p-2 h-[60px]"></div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
+        <div className="mx-auto w-full">
+          <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+            <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+              <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 opacity-90 rounded animate-pulse shadow mb-2 rounded-md h-[100px]">
+                <h3 className="text-lg leading-6 font-medium text-white">
+                  Loading ...
+                </h3>
+                <div className="mt-1 max-w-2xl text-sm text-gray-200 italic">
+                  {""}
+                  <div className="text-blue-500 underline hover:text-blue-700">
+                    {""}
+                  </div>
+                </div>
+                <div className="mt-1 max-w-2xl text-sm text-gray-200 italic">
+                  <div className="text-blue-500 underline hover:text-blue-700">
+                    {""}
+                  </div>
+                </div>
+              </li>
+              <li className="border-gray-100 flex flex-row mb-2">
+                <div className="w-full shadow select-none bg-gray-800 opacity-90 rounded animate-pulse rounded-md p-2 md:p-4 h-[100px]">
+                  <div className="px-1 flex flex-col">
+                    <div className="font-medium text-white">{""}</div>
+                  </div>
+                  <div className="w-full flex flex-column justify-start w-full bg-gray-500 text-gray-100 rounded-md my-4 p-2 h-[60px]"></div>
+                </div>
+              </li>
+              <li className="border-gray-100 flex flex-row mb-2">
+                <div className="w-full shadow select-none bg-gray-800 opacity-90 rounded animate-pulse rounded-md p-2 md:p-4 h-[100px]">
+                  <div className="px-1 flex flex-col">
+                    <div className="font-medium text-white">{""}</div>
+                  </div>
+                  <div className="w-full flex flex-column justify-start w-full bg-gray-500 text-gray-100 rounded-md my-4 p-2 h-[60px]"></div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-      </>
+      </Layout>
     );
   };
 
@@ -240,63 +218,49 @@ const NewResponse = () => {
       }
       // render the response
       return (
-        <>
+        <Layout>
           <Helmet helmetData={helmetData}>
             <title>New Response - Winner</title>
           </Helmet>
-          <div className="bg-indigo-900 relative overflow-hidden h-screen">
-            <img
-              src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-              className="absolute h-full w-full object-cover"
-            />
-            <Header hideHeader={hideHeader} />
-            <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
-            <div
-              ref={ref}
-              className="relative py-[74px] h-screen overflow-auto"
-              onScroll={(e) => handleScroll(e)}
-            >
-              <div className="mx-auto w-full">
-                <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-                  <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                    <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
-                      <h3 className="text-lg leading-6 font-medium text-white">
-                        {survey.title}
-                      </h3>
-                      <p className="mt-1 max-w-2xl text-sm text-gray-200 italic">
-                        Author:{" "}
-                        <Link
-                          className="text-blue-500 underline hover:text-blue-700"
-                          to={`/users/${survey.author_id}`}
-                        >
-                          {survey.author_name}
-                        </Link>
-                      </p>
-                      <p className="mt-1 max-w-2xl text-sm text-gray-200 italic">
-                        <Link
-                          className="text-blue-500 underline hover:text-blue-700"
-                          to={`/surveys/${survey.id}/responses`}
-                        >
-                          View all responses to this survey
-                        </Link>
-                      </p>
-                    </li>
-                    {renderQuestions()}
-                    <li>
-                      <button
-                        type="submit"
-                        className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                        onClick={handleSubmit}
-                      >
-                        Submit
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+          <div className="mx-auto w-full">
+            <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+              <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                <li className="px-4 py-5 sm:px-6 w-full border bg-gray-800 shadow mb-2 rounded-md">
+                  <h3 className="text-lg leading-6 font-medium text-white">
+                    {survey.title}
+                  </h3>
+                  <p className="mt-1 max-w-2xl text-sm text-gray-200 italic">
+                    Author:{" "}
+                    <Link
+                      className="text-blue-500 underline hover:text-blue-700"
+                      to={`/users/${survey.author_id}`}
+                    >
+                      {survey.author_name}
+                    </Link>
+                  </p>
+                  <p className="mt-1 max-w-2xl text-sm text-gray-200 italic">
+                    <Link
+                      className="text-blue-500 underline hover:text-blue-700"
+                      to={`/surveys/${survey.id}/responses`}
+                    >
+                      View all responses to this survey
+                    </Link>
+                  </p>
+                </li>
+                {renderQuestions()}
+                <li>
+                  <button
+                    type="submit"
+                    className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </li>
+              </ul>
             </div>
           </div>
-        </>
+        </Layout>
       );
     } else {
       // no survey found; 404
