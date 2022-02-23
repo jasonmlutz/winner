39fe6b880a905bd361da16804c78f4eaf9bf70a3
Jason Lutz <jason.m.lutz@gmail.com>
2022-02-22 20:04:34 -0800

    Add initial loading display to SurveysContainer
    Note that this change required (as before) to change the initial
    state of surveys to null; this means that an earlier seen
    Each child in a list should have a unique "key" prop error
    was due to the initial render mapping over each element
    of {[]}, and [].id, the key value, is undefined.
    
    TODO - build the loading skeleton and no surveys exist displays.

--- app/javascript/components/Surveys/SurveysContainer.jsx
+++ app/javascript/components/Surveys/SurveysContainer.jsx
@@ -8,19 +8,16 @@ import Header from "../Header";
 import ScrollToTopButton from "../resources/ScrollToTopButton";
 
 const SurveysContainer = () => {
-  const [surveys, setSurveys] = useState([{}]);
+  const [surveys, setSurveys] = useState(null);
   const [showScrollTopButton, setShowScrollTopButton] = useState(false);
 
   const navigate = useNavigate();
   const ref = createRef();
 
   const [hideHeader, setHideHeader] = useState(false);
-  // var scrollTop = 0;
   function handleScroll(e) {
     setShowScrollTopButton(e.target.scrollTop > 300);
     setHideHeader(e.target.scrollTop > 300);
-    // setHideHeader(e.target.scrollTop > scrollTop);
-    // scrollTop = e.target.scrollTop;
   }
 
   useEffect(() => {
@@ -46,52 +43,66 @@ const SurveysContainer = () => {
     return;
   }, [navigate]);
 
-  return (
-    <>
-      <Helmet helmetData={helmetData}>
-        <title>Surveys - Winner</title>
-      </Helmet>
-      <div className="bg-indigo-900 relative overflow-hidden h-screen">
-        <img
-          src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
-          className="absolute h-full w-full object-cover"
-        />
-        <Header hideHeader={hideHeader} />
-        <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
-        <div
-          ref={ref}
-          className="relative py-[74px] h-screen overflow-auto"
-          onScroll={(e) => handleScroll(e)}
-        >
-          <div className="mx-auto w-full">
-            <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
-              <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                {surveys.map((survey) => (
-                  <li
-                    key={survey.id}
-                    className="border-gray-100 flex flex-row mb-2"
-                  >
-                    <Link className="w-full" to={`/surveys/${survey.id}`}>
-                      <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 rounded-md p-4">
-                        <div className="px-1 flex flex-col">
-                          <div className="font-medium text-white">
-                            {survey.title}
+  if (surveys) {
+    // fetch returned a possibly empty array
+    if (surveys.length) {
+      // a non-empty array was returned; render
+      return (
+        <>
+          <Helmet helmetData={helmetData}>
+            <title>Surveys - Winner</title>
+          </Helmet>
+          <div className="bg-indigo-900 relative overflow-hidden h-screen">
+            <img
+              src="https://raw.githubusercontent.com/Charlie85270/tail-kit/main/public/images/landscape/5.svg"
+              className="absolute h-full w-full object-cover"
+            />
+            <Header hideHeader={hideHeader} />
+            <ScrollToTopButton visible={showScrollTopButton} ref={ref} />
+            <div
+              ref={ref}
+              className="relative py-[74px] h-screen overflow-auto"
+              onScroll={(e) => handleScroll(e)}
+            >
+              <div className="mx-auto w-full">
+                <div className="pb-24 md:pt-12 px-2 md:px-6 flex flex-col items-center">
+                  <ul className="flex flex-col w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5">
+                    {surveys.map((survey) => (
+                      <li
+                        key={survey.id}
+                        className="border-gray-100 flex flex-row mb-2"
+                      >
+                        <Link className="w-full" to={`/surveys/${survey.id}`}>
+                          <div className="w-full transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-700 select-none bg-gray-800 rounded-md p-4">
+                            <div className="px-1 flex flex-col">
+                              <div className="font-medium text-white">
+                                {survey.title}
+                              </div>
+                              <div className="text-sm italic text-gray-100 my-2">
+                                Author: {survey.author_name}
+                              </div>
+                            </div>
                           </div>
-                          <div className="text-sm italic text-gray-100 my-2">
-                            Author: {survey.author_name}
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+      );
+    } else {
+      // no surveys returned
+      // TODO style no surveys exist message
+      return <>NO SURVEYS EXIST</>;
+    }
+  } else {
+    // fetch in progress or bad server error; render loading
+    // TODO style loading
+    return <>LOADING</>;
+  }
 };
 
 export default SurveysContainer;
