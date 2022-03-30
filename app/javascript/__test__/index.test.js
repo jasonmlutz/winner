a544fdb188800b88c1023ac23182f16be72265e3
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-30 10:11:07 -0700

    Create first passing test

--- app/javascript/__test__/index.test.js
+++ app/javascript/__test__/index.test.js
@@ -1,8 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../App";
+
+import { BrowserRouter } from "react-router-dom";
+import App from "../components/App";
+import { CurrentUserProvider } from "../components/contexts/CurrentUserContext";
 
 jest.mock("react-dom", () => ({ render: jest.fn() }));
+jest.mock("../components/App", () => {
+  return function App() {
+    return <div>APP</div>;
+  };
+});
 
 describe("Application root", () => {
   it("should render without crashing", () => {
@@ -12,7 +23,11 @@ describe("Application root", () => {
     require("../index.jsx");
     expect(ReactDOM.render).toHaveBeenCalledWith(
       <React.StrictMode>
-        <App />
+        <BrowserRouter>
+          <CurrentUserProvider>
+            <App />
+          </CurrentUserProvider>
+        </BrowserRouter>
       </React.StrictMode>,
       div
     );
